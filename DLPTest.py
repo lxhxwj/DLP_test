9965b3824528dc80b64eaa3dc6fafc8a6cffdec1
[lxhxwj] <[lxh_xwj@126.com]>
2019-12-12 14:29:11 +0800

    test

--- DLPTest.py
+++ DLPTest.py
@@ -3,10 +3,10 @@ import sys
  
 class DLPTest:
     def test_https(self):
-        fp = open("D:\\ride\\DLP_test\\test_https.txt", "a")
+        fp = open("test_https.txt", "a")
         fp.write("this is the first keyword\r\n")
         fp.close()
     def test_http(self):
-        fp = open("D:\\ride\\DLP_test\\test_http.txt", "a")
+        fp = open("test_http.txt", "a")
         fp.write("this is the first keyword\r\n")
         fp.close()
